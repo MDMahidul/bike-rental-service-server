cfeef8e9a7216d1e88019163c9e0ebb2aa88aa16
MDMahidul <mmbmahidul007@gmail.com>
2024-06-16 16:00:23 +0600

    fixed few errors

--- src/app/modules/bike/bike.validation.ts
+++ src/app/modules/bike/bike.validation.ts
@@ -1,24 +1,30 @@
 import { z } from 'zod';
 
 const createBikeValidationSchema = z.object({
-  name: z.string({ required_error: 'Name is required!' }).trim(),
-  description: z.string({ required_error: 'Description is required!' }).trim(),
-  pricePerHour: z.number({ required_error: 'Price per hour is required!' }),
-  isAvailable: z.boolean().default(true),
-  cc: z.number({ required_error: 'CC is required!' }),
-  year: z.number({required_error:'Year is required!'}),
-  model: z.string({required_error:'Bike model is required!'}),
-  brand: z.string({required_error:'Bike brand is required!'}),
+  body: z.object({
+    name: z.string({ required_error: 'Name is required!' }).trim(),
+    description: z
+      .string({ required_error: 'Description is required!' })
+      .trim(),
+    pricePerHour: z.number({ required_error: 'Price per hour is required!' }),
+    isAvailable: z.boolean().default(true),
+    cc: z.number({ required_error: 'CC is required!' }),
+    year: z.number({ required_error: 'Year is required!' }),
+    model: z.string({ required_error: 'Bike model is required!' }),
+    brand: z.string({ required_error: 'Bike brand is required!' }),
+  }),
 });
 
 const updateBikeValidationSchema = z.object({
-  name: z.string().trim().optional(),
-  description: z.string().trim().optional(),
-  pricePerHour: z.number().optional(),
-  cc: z.number().optional(),
-  year: z.number().optional(),
-  model: z.string().optional(),
-  brand: z.string().optional(),
+  body: z.object({
+    name: z.string().trim().optional(),
+    description: z.string().trim().optional(),
+    pricePerHour: z.number().optional(),
+    cc: z.number().optional(),
+    year: z.number().optional(),
+    model: z.string().optional(),
+    brand: z.string().optional(),
+  }),
 });
 
 export const bikeValidations = {
--- src/app/modules/booking/booking.validation.ts
+++ src/app/modules/booking/booking.validation.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 const bookingValidationSchema = z.object({
-  userId: z.string({ required_error: 'User ID is required!' }),
-  bikeId: z.string({ required_error: 'Bike ID is required!' }),
-  startTime: z.date({ required_error: 'Start time is required!' }),
-  returnTime: z.date({ required_error: 'Return time is required!' }),
-  totalCost: z.number({ required_error: 'Total cost is required!' }),
-  isReturned: z.boolean().default(false),
+  body: z.object({
+    userId: z.string({ required_error: 'User ID is required!' }),
+    bikeId: z.string({ required_error: 'Bike ID is required!' }),
+    startTime: z.date({ required_error: 'Start time is required!' }),
+    returnTime: z.date({ required_error: 'Return time is required!' }),
+    totalCost: z.number({ required_error: 'Total cost is required!' }),
+    isReturned: z.boolean().default(false),
+  }),
 });
 
 export const bookingValidations = {bookingValidationSchema}
